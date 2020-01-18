e4a57c943e4e259cb5e4496fa7b809f8e98af1f2
DomiDre <homiedomi@gmail.com>
2020-01-18 20:22:16 +0100

    Add comments

--- src/neutron_sf.rs
+++ src/neutron_sf.rs
@@ -1,11 +1,11 @@
 use crate::UncertainFloat;
 
 pub struct NeutronScatteringFactor {
-    pub b_c: UncertainFloat,
-    pub b_p: Option<UncertainFloat>,
+    pub b_c: UncertainFloat, //bound coherent scattering length (in fm)
+    pub b_p: Option<UncertainFloat>, 
     pub b_m: Option<UncertainFloat>,
-    pub bound_coherent_scattering_xs: Option<UncertainFloat>,
-    pub bound_incoherent_scattering_xs: Option<UncertainFloat>,
-    pub total_bound_scattering_xs: Option<UncertainFloat>,
-    pub absorption_xs: Option<UncertainFloat>
+    pub bound_coherent_scattering_xs: Option<UncertainFloat>, //bound coherent scattering cross section (in barn)
+    pub bound_incoherent_scattering_xs: Option<UncertainFloat>, //bound incoherent scattering cross section (in barn)
+    pub total_bound_scattering_xs: Option<UncertainFloat>, //total bound scattering cross section (in barn)
+    pub absorption_xs: Option<UncertainFloat> //absorption cross section for 2200 m/s neutrons  (in barn)
 }
