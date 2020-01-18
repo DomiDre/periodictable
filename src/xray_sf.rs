e29a35d1b5646579aa68c2f39b58117d0be08073
DomiDre <homiedomi@gmail.com>
2020-01-18 20:22:41 +0100

    Implement fn to perform linear interpolation for f1,f2

--- src/xray_sf.rs
+++ src/xray_sf.rs
@@ -7,3 +7,48 @@ pub struct AtomicScatteringFactor {
 pub struct XrayScatteringFactor {
     pub table: Vec<AtomicScatteringFactor> // E, f1, f2
 }
+
+impl XrayScatteringFactor {
+
+    /// For a given energy E, return linear interpolation of f1 and f2 from table 
+    pub fn get_f_at_E(&self, E: f64) -> Option<(Option<f64>, Option<f64>)> {
+        if let Some(idx) = self.table.iter().position(|x| x.energy > E) {
+            if idx == self.table.len() - 1  || idx == 0 {
+                let edge_value = &self.table[idx];
+                Some((edge_value.f1, edge_value.f2))
+            } else {
+                let lower = &self.table[idx-1];
+                let higher = &self.table[idx];
+                let f1_value = if let Some(f1_lower) = lower.f1 {
+                    if let Some(f1_higher) = higher.f1 {
+                        Some(
+                            f1_lower + 
+                            (f1_higher - f1_lower)*
+                            (E - lower.energy)/(higher.energy - lower.energy)
+                        )
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+                let f2_value = if let Some(f2_lower) = lower.f2 {
+                    if let Some(f2_higher) = higher.f2 {
+                        Some(
+                            f2_lower + 
+                            (f2_higher - f2_lower)*
+                            (E - lower.energy)/(higher.energy - lower.energy)
+                        )
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                };
+                Some((f1_value, f2_value))
+            }
+        } else {
+            None
+        }
+    }
+}
